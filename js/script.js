b8312ddf4e76242139dc92a40bbec73016e87ac4
RicardoCCossa <ricardoccossa@gmail.com>
2025-06-25 18:41:47 +0200

    navbar remove

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
+ /*================ toggle icon navbar ================= */
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-
 }
 
-
-
+ /*================ scroll sections active link ================= */
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -26,9 +25,12 @@ window.onscroll = () => {
             });
         };
     });
-
+    /*================ sticky navbar ================= */
     let header = document.querySelector('header');
-
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*================ remove toggle icon and navbar when click navbar liink (scroll)================= */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
